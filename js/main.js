49a08fb610cdc5f907d38130962e5aed3b194f9d
mayfielc <christopher.mayfield72@gmail.com>
2019-08-06 22:36:54 -0500

    function flip added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//state
+const cards = document.querySelectorAll('.memory-card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+    this.classList.toggle('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log([firstCard, secondCard]);
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
